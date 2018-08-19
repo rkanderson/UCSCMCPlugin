595049edcb9bdf931534b1ff2f3095965ad8d82e
Ryan Anderson <shnoutyanderson@gmail.com>
2018-08-19 09:44:08 -0700

    Stevenson has speed boost

--- src/mainPkg/UCSCPluginMain.java
+++ src/mainPkg/UCSCPluginMain.java
@@ -1,6 +1,9 @@
 package mainPkg;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class UCSCPluginMain extends JavaPlugin {
 	public static FileConfiguration config;
@@ -23,11 +26,30 @@ public class UCSCPluginMain extends JavaPlugin {
     	this.getCommand("college").setExecutor(new CommandCollege(this));
     	
         getServer().getPluginManager().registerEvents(new MyListener(this), this);
+        
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+            	updateStates();
+            }
+        }, 0L, 100L);
     }
     
     // Fired when plugin is disabled.
     @Override
     public void onDisable() {
 
+    }
+    
+    // Can handle anything that requires frequent checking.
+    // E.g. effects different colleges or classes require.
+    private void updateStates(){
+    	
+    	for(Player p: getServer().getOnlinePlayers()){
+    		int collegeIndex = playerDataHelper.getInt(p.getName(), "college");
+    		if(collegeIndex==0) { //Stevenson
+    			p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 200, 2));
+    		}
+    	}
     }
 }
